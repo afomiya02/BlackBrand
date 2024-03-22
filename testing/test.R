5aa8aaad7837f4d12fc14b1ce59181d2b3c21fc8
afomiya02 <afomiya@vt.edu>
2024-03-21 22:08:24 -0400

    interactive localities plot

--- testing/test.R
+++ testing/test.R
@@ -2,7 +2,10 @@ library(readxl)
 library(tidyverse)
 library(sf)
 library(tigris)
-setwd("C:/Users/Tim/Desktop/School/cmda4864/testing")
+library(ggplot2)
+library(ggtext)
+library(plotly)
+setwd("C:/Users/afomiyaa/Desktop/testrepo/testing")
 
 ## ALL RACE & AGE DEMOGRAPHICS FOUND IN THE 5-YEAR DP05 TABLE 
 ## IN THE AMERICAN CENSUS SURVEY TABLES
@@ -15,29 +18,29 @@ setwd("C:/Users/Tim/Desktop/School/cmda4864/testing")
 #   small dataframe with the locality, age and race estimates/percentages
 get_demographics <- function(filename) {
   df <- read.csv(filename) %>%
-    `colnames<-`(c("label", "estimate", "moe", "pct_estimate", "pct_moe")) %>% # rename column names for automation purpose
-    mutate(., across(.cols = everything(), tolower)) %>% # make everything lowercase
-    mutate_all(str_replace_all, " ", "_") %>% # make everything lowercase
-    mutate_all(str_replace_all, " |[()]", "") %>% # remove parentheses
-    mutate_all(str_trim) %>% # trim whitespace
-    # NOTE: IF YOU WANT TO ADD MORE COLUMNS YOU NEED TO MANUALLY CHANGE THE COLUMN NAMES
-    filter(label %in% c("total_population", "median_age_years", "black_or_african_american")) %>% # filter median age and % of blacks in population
-    # MANUALLY UPDATE THIS TOO
-    slice(c(1, 2, 6)) %>% # median age, % black (one or two races)
-    mutate(loc = str_extract(filename, "(?<=/)[^.]+(?=\\.)"), .before = 1) # slices everything before .csv
+    `colnames<-`(c("label", "estimate", "moe", "pct_estimate", "pct_moe")) %>%
+    mutate(., across(.cols = everything(), tolower)) %>%
+    mutate_all(str_replace_all, " ", "_") %>%
+    mutate_all(str_replace_all, " |[()]", "") %>%
+    mutate_all(str_trim) %>%
+    filter(label %in% c("total_population", "median_age_years", "black_or_african_american")) %>%
+    slice(c(1, 2, 6)) %>%
+    mutate(loc = str_extract(filename, "(?<=/)[^.]+(?=\\.)"), .before = 1) %>%
+    select(-c(moe, pct_estimate, pct_moe)) # Remove unnecessary columns
+  
+  # Reshape the data
+  df <- pivot_wider(data = df, names_from = label, values_from = estimate)
+  
   return(df)
 }
 
-# loop that merges all 16 hampton roads census data
+# Test the function
 filenames <- list.files("data", pattern = "*.csv", full.names = TRUE)
-race_age_data <- list()
-for (i in filenames) {
-  temp <- get_demographics(i)
-  race_age_data[[i]] <- temp
-}
+race_age_data <- lapply(filenames, get_demographics)
 race_age_data <- bind_rows(race_age_data)
 
-
+# Print the result
+print(race_age_data)
 # figuring out heatmap stuff
 
 geo_data <- readRDS('./data/geo_data.rds')
@@ -47,7 +50,22 @@ counties <- counties(state = "Virginia", cb = TRUE) %>%
 
 ggplot(data = geo_data) + geom_sf()
 
-## READ ME:
+leaflet() %>% # Add county polygons
+  #addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(
+    data = geo_data,
+    fillColor = "red",
+    color = "black",
+    weight = 1,
+    popup = paste(
+      "<b>County:</b>", race_age_data$loc,
+      "<br><b>Median Age:</b>", race_age_data$median_age_years,
+      "<br><b>Number of Black People:</b>", race_age_data$black_or_african_american
+    )
+  )
+
+
+
 # there's a likelihood you're gonna need to change the function because i believe that the 
 # percent estimates for the black population are NOT numeric -- that's a simple fix
 
